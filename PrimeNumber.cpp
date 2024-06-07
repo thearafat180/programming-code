553e134acde6ad27006371511d13bc3e6312ab67
Arafat Rahman <139876123+thearafat180@users.noreply.github.com>
2024-06-07 07:16:31 +0600

    Update PrimeNumber.cpp
    Code_1 & Code_2 : Check the number is prime or not.
    Code_3 : Print the prime numbers between two numbers.

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -1,7 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Is prime or not.
+
+
+
+
+// Check the number is prime or not.
 
 int main(){
   int n,i;
@@ -16,5 +20,55 @@ int main(){
   if(i==n){
     cout << "The number is Prime." << endl;
   }
+  return 0;
+}
+
+
+
+
+// Another tecnique but previous was better
+
+int main(){
+  int n,test=1;
+  cout << "Type your number = ";
+  cin >> n;
+  for(int i=2; i<n; i++){
+    if(n%i==0){
+      test = 0;
+      break;
+    }
+  }
+  if(test==1){
+    cout << "The number is Prime." << endl;
+  }
+  else{
+    cout << "The number is Non-Prime." << endl;
+  }
+  return 0;
+}
+
+
+
+
+
+
+// Print prime numbers between two numbers.
+
+int main(){
+  int i,a,b,count=0;
+  cout << "Type the range = ";
+  cin >> a >> b;
+  for(a; a<=b; a++){
+    for(i=2; i<a; i++){
+      if(n%i==0){
+        break;
+      }
+    }
+    if(i==a){
+      cout << i << endl;
+      count++;
+    }
+  }
+  cout << "There are " << count << " prime numbers between " << a << " and " << b;
   return 0;
 }
