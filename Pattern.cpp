11608ee84166f78ad33a13c571c4e9a1b415b5a4
Arafat Rahman <139876123+thearafat180@users.noreply.github.com>
2024-06-09 05:47:22 +0600

    Update Pattern.cpp

--- Pattern.cpp
+++ Pattern.cpp
@@ -382,12 +382,19 @@ int main(){
   int n;
   cout << "Type here a number = ";
   cin >> n;
-  for(int i=1; i<=n; i++){
+  int num=1;
+  for(int i=0; i<n; i++){
     for(int j=i; j<n; j++){
       cout << "  ";
     }
-    for(int j=1; j<=i; j++){
-      cout << j << " ";
+    for(int j=0; j<=i; j++){
+        if(j==0 || i==0){
+            num =1;
+        }
+        else{
+            num= num*(i-j+1)/j;
+        }
+        cout << num  << "   ";
     }
     cout << endl;
   }
