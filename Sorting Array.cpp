3e286d720af7d81d28f8b7815cb0e2d9eda1be43
Arafat Rahman <139876123+thearafat180@users.noreply.github.com>
2024-06-16 04:15:08 +0600

    Update Sorting Array.cpp

--- Sorting Array.cpp
+++ Sorting Array.cpp
@@ -56,7 +56,7 @@ int main(){
   // Bubble Sorting
 
   for(int i=1; i<n; i++){
-    for(int j=0; j<n; j++){
+    for(int j=0; j<n-i; j++){
       if(arr[j]>arr[j+1]){
         int temp = arr[j];
         arr[j]= arr[j+1];
