dc70c1199a696d1676a65babd5ab60c5a7c64c9a
Arafat Rahman <139876123+thearafat180@users.noreply.github.com>
2024-06-15 22:50:41 +0600

    Update Sorting Array.cpp

--- Sorting Array.cpp
+++ Sorting Array.cpp
@@ -34,3 +34,40 @@ int main(){
   }
   return 0;
 }
+
+
+
+// Bubble Sorting
+
+int main(){
+  int n;
+  cout << "Type the size of an array = ";
+  cin >> n;
+  int arr[n];
+  for(int i=0; i<n; i++){
+    cout << "Elemnent " << i+1 << " = ";
+    cin >> arr[i];
+  }
+  cout << "The Array = ";
+  for(int i=0; i<n; i++){
+    cout << arr[i] << " ";
+  }
+  
+  // Bubble Sorting
+
+  for(int i=1; i<n; i++){
+    for(int j=0; j<n; j++){
+      if(arr[j]>arr[j+1]){
+        int temp = arr[j];
+        arr[j]= arr[j+1];
+        arr[j+1]=temp;
+      }
+    }
+  }
+  cout << "\nThe Sorting Array = ";
+  for(int i=0; i<n; i++){
+    cout << arr[i] << " ";
+  }
+  return 0;
+}
+  
