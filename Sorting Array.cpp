51e43468a6d6e417ece4cbd4ac6f81189560bc27
Arafat Rahman <139876123+thearafat180@users.noreply.github.com>
2024-06-18 06:57:44 +0600

    Update Sorting Array.cpp

--- Sorting Array.cpp
+++ Sorting Array.cpp
@@ -70,4 +70,43 @@ int main(){
   }
   return 0;
 }
+
+
+
+
+
+
+
+// Insertion Sort
+
+int main(){
+  int n;
+  cout << "Type the size of an array = ";
+  cin >> n;
+  int arr[n];
+  for(int i=0; i<n; i++){
+    cout << "Elemnent " << i+1 << " = ";
+    cin >> arr[i];
+  }
+  cout << "The Array = ";
+  for(int i=0; i<n; i++){
+    cout << arr[i] << " ";
+  }
+  
+  // Insertion Sorting
+
+  for(int i=1; i<n; i++){
+    for(int j=i-1; j>=0 && arr[j+1]<arr[j]; j--){
+      int temp = arr[j];
+      arr[j] = arr[j+1];
+      arr[j+1] = temp;
+    }
+  }
+  cout << "\nThe Sorting Array = ";
+  for(int i=0; i<n; i++){
+    cout << arr[i] << " ";
+  }
+  return 0;
+}
+
   
